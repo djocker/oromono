7a7e64bd7086ef4408cb537893eb927286e254f3
SergeyCherepanov <sergey@cherepanov.org.ua>
2017-01-20 14:50:54 +0400

    fixed: mysql check fixed: www folder owner check

--- bin/run.sh
+++ bin/run.sh
@@ -7,7 +7,7 @@ function info {
 }
 
 # Check and fix ownership if invalid
-if [[ `stat -c '%u:%g' /var/www` -ne `getent passwd | grep www-data | awk -F ':' '{print $3 ":" $4}'` ]] || [[ `ls -l ${APP_ROOT} | awk '{print $3 ":" $4}' | grep -v www-data:www-data | wc -l` -gt 0 ]]; then
+if [[ `stat -c '%u:%g' /var/www` != `getent passwd | grep www-data | awk -F ':' '{print $3 ":" $4}'` ]] || [[ `ls -l ${APP_ROOT} | awk '{print $3 ":" $4}' | grep -v www-data:www-data | wc -l` -gt 0 ]]; then
     info "Fix ownership for /var/www  directory"
     chown -R $(getent passwd | grep www-data | awk -F ':' '{print $3 ":" $4}') ${APP_ROOT}
 fi
@@ -48,17 +48,6 @@ if [[ -z ${IS_LOCAL} ]]; then
     runuser -s /bin/sh -c "ln -s ${DATA_ROOT}/attachment  ${APP_ROOT}/app/attachment" www-data
 fi
 
-info "Checking if application is already installed"
-if [[ ! -z ${APP_IS_INSTALLED} ]] \
-    || [[ `mysql -e "show databases like '${APP_DB_NAME}'" -h${APP_DB_HOST} -u${APP_DB_USER} -p${APP_DB_PASSWORD} -N | wc -l` -gt 0 ]] \
-    && [[ `mysql -e "show tables from ${APP_DB_NAME}" -h${APP_DB_HOST} -u${APP_DB_USER} -p${APP_DB_PASSWORD} -N | wc -l` -gt 0 ]]; then
-  sed -i -e "s/installed:.*/installed: true/g" /var/www/app/config/parameters.yml
-  info "Application is already installed!"
-  APP_IS_INSTALLED=true
-else
-  info "Application is not installed!"
-fi
-
 if [[ -z ${APP_DB_PORT} ]]; then
     if [[ "pdo_pgsql" = ${APP_DB_DRIVER} ]]; then
         APP_DB_PORT="5432"
@@ -71,6 +60,18 @@ until nc -z ${APP_DB_HOST} ${APP_DB_PORT}; do
     info "Waiting database on ${APP_DB_HOST}:${APP_DB_PORT}"
     sleep 2
 done
+
+info "Checking if application is already installed"
+if [[ ! -z ${APP_IS_INSTALLED} ]] \
+    || [[ `MYSQL_PWD="${APP_DB_PASSWORD}" mysql -e "show databases like '${APP_DB_NAME}'" -h${APP_DB_HOST} -u${APP_DB_USER} -N | wc -l` -gt 0 ]] \
+    && [[ `MYSQL_PWD="${APP_DB_PASSWORD}" mysql -e "show tables from ${APP_DB_NAME}" -h${APP_DB_HOST} -u${APP_DB_USER} -N | wc -l` -gt 0 ]]; then
+  sed -i -e "s/installed:.*/installed: true/g" /var/www/app/config/parameters.yml
+  info "Application is already installed!"
+  APP_IS_INSTALLED=true
+else
+  info "Application is not installed!"
+fi
+
 
 if [[ ! -z ${CMD_INIT_BEFORE} ]]; then
     info "Running pre init command: ${CMD_INIT_BEFORE}"
